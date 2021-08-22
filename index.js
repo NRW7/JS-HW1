5074c2e8dcf57d50dca32796d580c9893cb775a0
N Wry <85411744+NRW7@users.noreply.github.com>
2021-08-22 11:24:31 -0600

    JS-HW1

---
+++ index.js
@@ -0,0 +1,29 @@
+var num = 2
+var num2 = 7
+
+console.log(num + num2)
+
+var num3 = 4
+var num4 = 5
+
+console.log(num3 - num4)
+
+var num5 = 9
+var num6 = 3
+
+console.log(num5 % num6)
+
+var num7 = 6
+var num8 = 8
+
+console.log(num7 * num8)
+
+var greet = "Hello "
+var earth = "World!"
+
+console.log(greet + earth)
+
+var my = "My favorite food is "
+var food = "a smothered spicy ranch pizza burrito."
+
+console.log(my + food)
